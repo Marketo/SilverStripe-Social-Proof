9a6fc6cc2c4076e55e43e55705c72f5281274426
Kirk Mayo <kirk@stripetheweb.com>
2015-12-01 12:24:34 +1300

    Setup a process queue for the Linkedin service

--- code/services/LinkedinCount.php
+++ code/services/LinkedinCount.php
@@ -13,17 +13,38 @@ class LinkedinCount extends SocialServiceCount implements SocialServiceInterface
 
     function getLinkedInCall() {
         return 'http://www.linkedin.com/countserv/count/share?url='
-            . urlencode($this->entry->URL);
+            . urlencode($this->entry['URL']);
     }
-    function getCount(){
+    function processQueue(){
         try {
-            $fileData = file_get_contents($this->getLinkedInCall());
-            $output = str_replace(array('IN.Tags.Share.handleCount(',');'),'',trim($fileData));
-            if($output === FALSE) return 0;
-
-            $json = json_decode($output);
-            unset($fileData); // free memory
-            return intval($json->count);
+            foreach ($this->queue as $entry) {
+                $this->entry = $entry;
+                $fileData = file_get_contents($this->getLinkedInCall());
+                $output = str_replace(array('IN.Tags.Share.handleCount(',');'),'',trim($fileData));
+                if($output !== FALSE) {
+                    $json = json_decode($output);
+                    unset($fileData); // free memory
+                    $count = intval($json->count);
+                    $id = $entry['ID'];
+                    $entry = SocialQueue::get_by_id('SocialQueue',$id);
+                    $statistic = URLStatistics::get()
+                        ->filter(array(
+                            'URLID' => $entry->URLID,
+                            'Service' => $this->service,
+                            'Action' => $this->statistic
+                        ))->first();
+                    if (!$statistic || !$statistic->exists()) {
+                        $statistic = URLStatistics::create();
+                        $statistic->URLID = $entry->URLID;
+                        $statistic->Service = $this->service;
+                        $statistic->Action = $this->statistic;
+                    }
+                    $statistic->Count = $count;
+                    $statistic->write();
+                    $entry->Queued = 0;
+                    $entry->write();
+                }
+            }
 
         } catch (Exception $e) {
             return 0;

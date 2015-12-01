7286e5f8c9303a653d8bad7a4b2529e3a6dd7de0
Kirk Mayo <kirk@stripetheweb.com>
2015-12-01 13:59:00 +1300

    Seetup a process queue for Google plus

--- code/services/GooglePlusCount.php
+++ code/services/GooglePlusCount.php
@@ -11,24 +11,44 @@ class GooglePlusCount extends SocialServiceCount implements SocialServiceInterfa
     public $service = 'Google';
     public $statistic = 'count';
 
-    function getCount(){
+    function processQueue(){
         try {
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"'
-                . $this->entry->URL .
-                '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-            $curl_results = curl_exec ($curl);
+            foreach ($this->queue as $entry) {
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
+                curl_setopt($curl, CURLOPT_POST, 1);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"'
+                    . $entry['URL'] .
+                    '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+                $curl_results = curl_exec ($curl);
 
-            if(curl_errno($curl)) return 0;
-            curl_close ($curl);
+                if(curl_errno($curl)) return 0;
+                curl_close ($curl);
 
-            $json = json_decode($curl_results, true);
+                $json = json_decode($curl_results, true);
 
-            return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
+                $count = intval( $json[0]['result']['metadata']['globalCounts']['count'] );
+                $id = $entry['ID'];
+                $entry = SocialQueue::get_by_id('SocialQueue',$id);
+                $statistic = URLStatistics::get()
+                    ->filter(array(
+                        'URLID' => $entry->URLID,
+                        'Service' => $this->service,
+                        'Action' => $this->statistic
+                    ))->first();
+                if (!$statistic || !$statistic->exists()) {
+                    $statistic = URLStatistics::create();
+                    $statistic->URLID = $entry->URLID;
+                    $statistic->Service = $this->service;
+                    $statistic->Action = $this->statistic;
+                }
+                $statistic->Count = $count;
+                $statistic->write();
+                $entry->Queued = 0;
+                $entry->write();
+            }
 
         } catch (Exception $e) {
             return 0;

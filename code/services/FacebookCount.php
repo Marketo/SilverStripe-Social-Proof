6cbf15def21cdfdd2a37fb67da5387c743304f2d
Kirk Mayo <kirk@stripetheweb.com>
2015-12-03 12:09:25 +1300

    Merged facebook services and added sanity check to make sure db entries exist

--- code/services/FacebookCount.php
+++ code/services/FacebookCount.php
@@ -9,12 +9,15 @@ class FacebookCount extends SocialServiceCount implements SocialServiceInterface
 
     public $entry;
     public $service = 'Facebook';
-    public $statistic = 'like_count';
+    public $statistic = array(
+        'share_count',
+        'like_count'
+    );
     public $requestCount = 5;
 
 	private function getFacebookCall($urls) {
         return 'https://api.facebook.com/method/fql.query' .
-            '?query=select%20url,like_count%20from%20link_stat%20where%20url%20in("'
+            '?query=select%20url,share_count,like_count%20from%20link_stat%20where%20url%20in("'
             . urlencode(implode('","', $urls)).'")';
     }
 
@@ -30,39 +33,70 @@ class FacebookCount extends SocialServiceCount implements SocialServiceInterface
                 $urls[$entry['ID']] = $entry['URL'];
                 if ($i == $this->requestCount || $step == $noEntries) {
                     $fileData = file_get_contents($this->getFacebookCall($urls));
-                    if($fileData === FALSE) return 0;
+                    if($fileData === FALSE) {
+                        $this->errorQueue[] = $url;
+                    } else {
 
-                    $xml = simplexml_load_string($fileData);
+                        $xml = simplexml_load_string($fileData);
 
-                    if ($xml->error_code || !$xml->link_stat) {
-                        foreach ($urls as $url) {
-                            $this->errorQueue[] = $url;
-                            continue;
+                        if ($xml->error_code || !$xml->link_stat) {
+                            foreach ($urls as $url) {
+                                $this->errorQueue[] = $url;
+                                continue;
+                            }
                         }
-                    }
-                    $results = $xml->link_stat;
-                    $ids = array_flip($urls);
-                    foreach ($results as $result) {
-                        $url = (string)$result->url;
-                        $id = $ids[$url];
-                        $entry = SocialQueue::get_by_id('SocialQueue',$id);
-                        $statistic = URLStatistics::get()
-                            ->filter(array(
-                                'URLID' => $entry->URLID,
-                                'Service' => $this->service,
-                                'Action' => $this->statistic
-                            ))->first();
-                        if (!$statistic || !$statistic->exists()) {
-                            $statistic = URLStatistics::create();
-                            $statistic->URLID = $entry->URLID;
-                            $statistic->Service = $this->service;
-                            $statistic->Action = $this->statistic;
+                        $results = $xml->link_stat;
+                        $ids = array_flip($urls);
+                        foreach ($results as $result) {
+                            $url = (string)$result->url;
+                            $id = $ids[$url];
+                            $entry = SocialQueue::get_by_id('SocialQueue',$id);
+                            $statistics = URLStatistics::get()
+                                ->filter(array(
+                                    'URLID' => $entry->URLID,
+                                    'Service' => $this->service,
+                                    'Action' => $this->statistic
+                                ));
+                            if ($statistics && $statistics->exists()) {
+                                foreach ($statistics as $statistic) {
+                                    $statistic->Count = (int)$result->{$statistic->Action};
+                                    $statistic->write();
+                                }
+                            } else {
+                                foreach($this->statistic as $countStat) {
+                                    $statistic = URLStatistics::create();
+                                    $statistic->URLID = $entry->URLID;
+                                    $statistic->Service = $this->service;
+                                    $statistic->Action = $countStat;
+                                    $statistic->Count = (int)$result->{$statistic->Action};
+                                    $statistic->write();
+                                }
+                            }
+                            // sanity check what should be in the db and what is actually in it
+                            if (count($this->statistic) != $statistics->count()) {
+                                foreach ($this->statistic as $statistic) {
+                                    if ($result->$statistic) {
+                                        // do we have this in the db
+                                        $stat = URLStatistics::get()
+                                            ->filter(array(
+                                            'URLID' => $entry->URLID,
+                                            'Service' => $this->service,
+                                            'Action' => $statistic
+                                        ))->first();;
+                                        if (!$stat || !$stat->exists()) {
+                                            $stat = URLStatistics::create();
+                                            $stat->URLID = $entry->URLID;
+                                            $stat->Service = $this->service;
+                                            $stat->Action = $statistic;
+                                            $stat->Count = (int)$result->{$statistic};
+                                            $stat->write();
+                                        } 
+                                    }
+                                }
+                            }
+                            $entry->Queued = 0;
+                            $entry->write();
                         }
-                        $statistic->Count = (int)$result->{$this->statistic};
-                        $statistic->write();
-                        $entry->Queued = 0;
-                        $entry->write();
-                        
                     }
 
                     unset($fileData); // free memory
